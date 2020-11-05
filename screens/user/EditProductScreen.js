7427f0d1b57f1f094ace3a0da81a4186a0b9b113
keremcanb <keremcan@gmail.com>
2020-11-05 23:05:16 +0300

    edit form

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -13,12 +13,94 @@ import { useSelector, useDispatch } from 'react-redux';
 import HeaderButton from '../../components/UI/HeaderButton';
 import * as productsActions from '../../store/actions/products';
 
-const EditProductScreen = (props) => {
-  return (
-    <View>
-      <Text>Edit product</Text>
-    </View>
+const EditProductScreen = ({ navigation }) => {
+  const productId = navigation.getParam('productId');
+
+  const editedProduct = useSelector((state) =>
+    state.products.userProducts.find((product) => product.id === productId)
   );
+
+  const [info, setInfo] = useState({
+    title: editedProduct ? editedProduct.title : '',
+    imageUrl: editedProduct ? editedProduct.imageUrl : '',
+    price: '',
+    description: editedProduct ? editedProduct.description : ''
+  });
+  const { title, imageUrl, price, description } = info;
+
+  // const submitHandler = (e) => {
+  //   e.preventDefault();
+  // };
+
+  const changeHandler = (e) =>
+    setInfo({ ...info, [e.target.name]: e.target.value });
+
+  return (
+    <ScrollView>
+      <View style={styles.form}>
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Title</Text>
+          <TextInput
+            style={styles.input}
+            name={title}
+            value={title}
+            onChange={changeHandler}
+          />
+        </View>
+
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Image Url</Text>
+          <TextInput
+            style={styles.input}
+            value={imageUrl}
+            name={imageUrl}
+            onChange={changeHandler}
+          />
+        </View>
+
+        {editedProduct ? null : (
+          <View style={styles.formControl}>
+            <Text style={styles.label}>Price</Text>
+            <TextInput
+              style={styles.input}
+              value={price}
+              name={price}
+              onChange={changeHandler}
+            />
+          </View>
+        )}
+
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Description</Text>
+          <TextInput
+            style={styles.input}
+            value={description}
+            name={description}
+            onChange={changeHandler}
+          />
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+EditProductScreen.navigationOptions = (navData) => {
+  return {
+    headerTitle: navData.navigation.getParam('productId')
+      ? 'Edit Product'
+      : 'Add Product',
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Save"
+          iconName={
+            Platform.OS === 'android' ? 'md-checkmark' : 'ios-checkmark'
+          }
+          onPress={() => {}}
+        />
+      </HeaderButtons>
+    )
+  };
 };
 
 const styles = StyleSheet.create({
